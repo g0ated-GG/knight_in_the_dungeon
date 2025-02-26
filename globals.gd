3afa2513361a66a219bb9e792e30948bfe95535c
Sergey Skokov <sergskokow@gmail.com>
2025-02-26 22:31:02 +0300

    Saving deaths and last checkpoint into the config file.

--- globals.gd
+++ globals.gd
@@ -1,3 +1,17 @@
 extends Node
 
-var deaths : int = 0
+var deaths : int = 0 :
+	set(value):
+		deaths = value
+		save('deaths', value)
+var checkpoint : int = 0 :
+	set(value):
+		checkpoint = value
+		save('checkpoint', value)
+
+func save(param_name : String, value) -> void:
+	var config = ConfigFile.new()
+	var config_path = ProjectSettings.globalize_path('res://save.cfg')
+	if config.load(config_path) == OK:
+		config.set_value('save', param_name, value)
+		config.save(config_path)
--- scenes/menu/main_menu.gd
+++ scenes/menu/main_menu.gd
@@ -1,7 +1,21 @@
 extends Node2D
 
+func _ready() -> void:
+	var config = ConfigFile.new()
+	if config.load(ProjectSettings.globalize_path('res://save.cfg')) == OK:
+		Globals.deaths = config.get_value('save', 'deaths')
+		Globals.checkpoint = config.get_value('save', 'checkpoint')
+	if Globals.checkpoint > 0 or Globals.deaths > 0:
+		$CanvasLayer/Buttons/ContinueButton.show()
+
+func _on_continue_button_pressed() -> void:
+	if Globals.checkpoint in range(0, 2):
+		get_tree().change_scene_to_file("res://scenes/world/checkpoint_%d.tscn" % Globals.checkpoint)
+
 func _on_play_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/world/level_1.tscn")
+	Globals.deaths = 0
+	Globals.checkpoint = 0
+	_on_continue_button_pressed()
 
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
